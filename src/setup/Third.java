6b288f7780694bc5c157b12cb939496a37721a0a
olhakobiuk <oliakobjuk0952@gmail.com>
2021-10-03 12:45:26 -0400

    created third file

---
+++ src/setup/Third.java
@@ -0,0 +1,11 @@
+package setup;
+
+public class Third {
+
+    public void hello(){
+        System.out.println("Hello world");
+    }
+    public void hello(String s){
+        System.out.println("Hello world 1");
+    }
+}
