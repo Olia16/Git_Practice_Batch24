29529bc5c06f6ad714e623414c0001f86d880292
olhakobiuk <oliakobjuk0952@gmail.com>
2021-10-03 12:24:14 -0400

    added to second file again

--- src/setup/Second.java
+++ src/setup/Second.java
@@ -3,14 +3,14 @@ package setup;
 public class Second {
 
     public void my(){
-
+        System.out.println("first method");
     }
     public void my(int a){
-
+        System.out.println("second method");
     }
 
     public void my(String s){
-
+        System.out.println("Third method");
     }
 
 }
