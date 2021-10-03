2f51216bdb7eb25d595f8fcf0bd9949f97daa901
olhakobiuk <oliakobjuk0952@gmail.com>
2021-10-03 15:27:46 -0400

    updating first file

--- src/setup/First.java
+++ src/setup/First.java
@@ -3,7 +3,7 @@ package setup;
 public class First {
     public static void main(String[] args) {
 
-        System.out.println("Hello world");
+        System.out.println("Hello Universe");
 
     }
 }
